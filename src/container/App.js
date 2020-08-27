455835ab9ea63a76feef610faca854c38dcc6fa9
Humzah <Humzah7khan@gmail.com>
2020-08-27 16:33:19 -0400

    missing bracket in mapDispatchToProps for the onRequestRobots line, async seems to be complete

--- src/container/App.js
+++ src/container/App.js
@@ -5,42 +5,36 @@ import SearchBox from '../components/SearchBox';
 import ErrorBoundry from '../components/Error Boundary';
 import './App.css';
 import Scroll from '../components/Scroll'
-import { setSearchField } from '../action'
+import { setSearchField, requestRobots } from '../action'
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
 }
 
 class App extends Component{
-    constructor(){
-        super()
-        this.state = {
-            robots: []
-        }
-    }
 
     componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({robots:users}))
-        
+        this.props.onRequestRobots();
     }
 
     render(){
-        const { robots } = this.state;
-        const { searchField, onSearchChange } = this.props;
+        const { searchField, onSearchChange, robots, isPending } = this.props;
         const filterRobots = robots.filter(robot=>{
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
-        return (robots.length === 0) ? <h1>Loading</h1> :
+        return isPending ? <h1>Loading</h1> :
         (
             <div className = 'tc'>
                 <h1 className = 'f2'>Robofriends</h1>
